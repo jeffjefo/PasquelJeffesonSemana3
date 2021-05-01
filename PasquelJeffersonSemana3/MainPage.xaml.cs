8c34b3d1508976635e224aa5709630d184fe4ce8
jeffjefo <jefojeff02@gmail.com>
2021-05-01 01:57:11 -0500

    TareaS3

---
+++ PasquelJeffersonSemana3/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PasquelJeffersonSemana3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //async para mostrar flecha de regreso
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            String usuario = txtUsuario.Text;
+            String clave = txtClave.Text;
+            //condicion
+            if (usuario == "estudiante2021" && clave == "uisrael2021")
+                //await ejecuta cada linea
+                //sale erro en page 2 porque se debe modificar los parametros en el page
+                await Navigation.PushAsync(new Page2(usuario, clave));
+            
+            else
+                await DisplayAlert("Advertencia", "¡Usuario o clave incorrectas!", "OK");
+
+        }
+    }
+}
---
+++ PasquelJeffersonSemana3/Page2.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PasquelJeffersonSemana3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        //  aqui añado los datos añadidos en el mainpage
+        public Page2(String usuario, String clave)
+        {
+            InitializeComponent();
+            //lblUsuario.Text = usuario;
+            //lblClave.Text = clave;
+        }
+
+        private void btnSumar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double dato1 = Convert.ToDouble(txtNota1.Text);
+                double dato2 = Convert.ToDouble(txtNota2.Text);
+                double dato3 = Convert.ToDouble(txtNota3.Text);
+                double dato4 = Convert.ToDouble(txtNota4.Text);
+                double nota1 = dato1 * 0.3;
+                double nota2 = dato2 * 0.2;
+                double nota3 = dato3 * 0.3;
+                double nota4 = dato4 * 0.2;
+                double parcial1 = nota1 + nota2;
+                double parcial2 = nota3 + nota4;
+                double total = parcial1 + parcial2;
+                if (total >= 7)
+                {
+                    txtEstado.Text = "APROBADO";
+                }
+                else
+                {
+                    txtEstado.Text = "REPROBADO";
+                }
+
+                txtNotaParcial1.Text = parcial1.ToString();
+                txtNotaParcial2.Text = parcial2.ToString();
+                txtNotaFinal.Text = total.ToString();
+
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Gracias");
+            }
+        }
+    }
+}
